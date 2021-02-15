04d49fbcc8268160b8b0372c72f048f2c97a2a62
Luiz C. M. de Aquino <aquino.luizclaudio@gmail.com>
2021-02-15 11:58:29 -0300

    Corrige versão da aplicação.

--- minhaacademia/app/MinhaAcademia.php
+++ minhaacademia/app/MinhaAcademia.php
@@ -15,7 +15,7 @@ class MinhaAcademia
      *
      * @var string
      */
-    protected const VERSION = '1.2.2';
+    protected const VERSION = '1.2.4';
 
     /**
      * Number of steps for updating process.
