4208d52682aa4d02c24ae8a966ef21add9fd44ea
Luiz C. M. de Aquino <aquino.luizclaudio@gmail.com>
2021-03-17 12:04:48 -0300

    Atualiza a versão.

--- minhaacademia/app/MinhaAcademia.php
+++ minhaacademia/app/MinhaAcademia.php
@@ -15,7 +15,7 @@ class MinhaAcademia
      *
      * @var string
      */
-    protected const VERSION = '1.2.5';
+    protected const VERSION = '1.2.6';
 
     /**
      * Number of steps for updating process.
